a49f93cb0d77ff2edac095f730a2708794d05cfd
Nada Stojanovic <nadastojanovic@Nadas-MacBook-Air.local>
2022-01-12 19:48:10 +0100

    Initial commit

---
+++ src/features/focus/Focus.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { RoundedButton } from '../../components/RoundedButton';
+import { fontSizes, paddingSizes } from '../../utils/sizes';
+import { colors } from '../../utils/colors';
+
+export const Focus = ({ addSubject }) => {
+  const [tmpItem, setTmpItem] = useState(null);
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>What would you like to focus on?</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onSubmitEditing={({ nativeEvent }) => {
+              setTmpItem(nativeEvent.text);
+            }}
+          />
+          <RoundedButton
+            size={60}
+            title="+"
+            onPress={() => {
+              addSubject(tmpItem);
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleContainer: {
+    flex: 0.5,
+    padding: paddingSizes.md,
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#FFEBC6',
+    fontWeight: 'bold',
+    fontSize: fontSizes.xxl,
+  },
+  input: {
+    backgroundColor: colors.yellow,
+    flex: 1,
+    marginRight: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
---
+++ src/utils/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+  darkBlue: '#003844',
+  lightBlue: '#006C67',
+  yellow: '#FFEBC6',
+};
